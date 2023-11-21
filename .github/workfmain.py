168aec57a1a9feb5539b043d43806a8f2cb1135b
Vishalsnw <151663162+Vishalsnw@users.noreply.github.com>
2023-11-21 22:36:04 +0530

    simple python code

---
+++ .github/workfmain.py
@@ -0,0 +1,8 @@
+# This is a simple Python script
+def greet(name):
+    return f"Hello, {name}!"
+
+# Test the function
+user_name = input("Enter your name: ")
+greeting = greet(user_name)
+print(greeting)
